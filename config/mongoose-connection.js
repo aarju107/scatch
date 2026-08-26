97d12ca6752a8d50264a05378ca7e8d1b39d765b
aarju 107 <aarjuchhimpa731@gmail.com>
2026-08-27 00:02:52 +0530

    mongodb

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,4 +1,16 @@
 const mongoose = require("mongoose");
 const config = require("config");
 
-mongoose.connect(config.get("MONGODB_URI"));
+// Prefer the Atlas URI from .env; fall back to config/development.json (local mongod)
+const uri = process.env.MONGODB_URI || config.get("MONGODB_URI");
+
+mongoose
+    .connect(uri)
+    .then(() => {
+        console.log(`MongoDB connected -> ${mongoose.connection.name}`);
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error:", err.message);
+    });
+
+module.exports = mongoose.connection;
